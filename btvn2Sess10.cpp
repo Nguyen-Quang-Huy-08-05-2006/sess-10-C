5fe2d27ef7c99c4f5bcf31649e97b2d022a173c0
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-12-03 13:17:19 +0700

    Update btvn2Sess10.cpp

--- btvn2Sess10.cpp
+++ btvn2Sess10.cpp
@@ -6,7 +6,7 @@ int main(){
 	for(int i=0;i<5;i++){
 		for(int j=0;j<5-i-1;j++){
 			if(arr[j]>arr[j+1]){
-				int o; //tao bien de doi vi tri, luu a vao c, luu b vao a, luu c vao b
+				int o;
 				o=arr[j];
 				arr[j]=arr[j+1];
 				arr[j+1]=o;
